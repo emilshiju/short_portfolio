fe5fcd07eef1bda8d599c1cee0762e6265edbe3d
emil shiju <emilshiju10@gmail.com>
2025-04-27 12:27:12 +0530

    final

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -13,13 +13,13 @@ const About = ()=>{
 
 <div className="text-[15px] lg:text-[17px] text-black  mt-10">
 <p>
-Hi, I'm Emil Shiju — a passionate self-taught Full Stack Developer with strong problem-solving skills and experience working with a wide range of tools and frameworks like MongoDB, Express, React, Node.js, Python, TypeScript, Next.js, PostgreSQL, GraphQL, WebRTC, Socket.io, and Git, with a special focus on the MERN stack.
+Hi, I'm Emil Shiju -  a passionate self-taught Full Stack Developer with strong problem-solving skills and experience working with a wide range of tools and frameworks like MongoDB, Express, React, Node.js, Python, TypeScript, Next.js, PostgreSQL, GraphQL, WebRTC, Socket.io, and Git, with a special focus on the MERN stack.
 </p>
 <p>
 After completing my higher secondary education, I built my coding skills through self-learning. I worked for long hours, dedicating months to developing my skills and successfully completing many industry-level projects. Through this journey, I gained real-world experience in building full-stack applications from scratch.
 </p>
 <p>
-For building projects, I used MVC and clean architecture to ensure structure and scalability, integrating them with AWS, Razorpay, Stripe, PayPal, Chargebee, S3, Cloudinary, Twilio, SendGrid, Firebase, NodeMailer, Zego Cloud, OAuth 2.0, JWT, Postman, and web scrapers.
+I have experienced in using MVC and clean architecture to ensure structure and scalability, integrating them with AWS, Razorpay, Stripe, PayPal, Chargebee, S3, Cloudinary, Twilio, SendGrid, Firebase, NodeMailer, Zego Cloud, OAuth 2.0, JWT, Postman, and web scrapers.
 </p>
 <p>
 Starting my journey as a self-learner, I developed the ability to quickly adapt to new technologies and libraries based on project needs. My strong research skills and independent learning mindset help me stay updated and find the best solutions for every challenge.
